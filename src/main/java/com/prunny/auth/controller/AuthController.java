a2c40136219ebb65429de7181d7641101b44dea0
Adejolu Temitope <tadejolu@gmail.com>
2024-07-02 13:54:39 +0100

    jwt service implementation

---
+++ src/main/java/com/prunny/auth/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.prunny.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class AuthController {
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello welcome to prunny Technology");
+    }
+
+}
